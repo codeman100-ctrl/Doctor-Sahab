658bd130d4a8b8cc4773040d5fe1f8c9a3552358
saihan <skilldevmr@gmail.com>
2025-08-10 01:16:50 +0300

    installed the dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='Doctor-Sahab',
+    version='0.1',  # Version of your package
+    author='codeman100-ctrl',
+    author_email='skilldevmr@gmail.com',
+    description='A complete medical chatbot using LLMs, LangChain, Pinecone, Flask, and AWS.',
+    packages=find_packages(),
+    install_requires=[]
+)
