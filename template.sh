f82a067602360ae801eaf26b00c5a03783e3f633
saihan <skilldevmr@gmail.com>
2025-08-10 00:47:34 +0300

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# create directories for the project structure
+mkdir -p src
+mkdir -p research
+
+#create files for the project
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompr.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
